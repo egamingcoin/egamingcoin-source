19158492fe6cb016081ec5d4416f5d517de83875
egamingcoin <info@egamingcoin.de>
2018-04-06 17:06:08 +0200

    Initial commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Egamingcoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "EgamingcoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
